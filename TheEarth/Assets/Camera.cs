ea868d0c3d05385f1dbaa5c708606e1ce388e407
MARY <phunmung@gmail.com>
2019-11-06 01:50:20 +0900

    エフェクト関係

---
+++ TheEarth/Assets/Camera.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    public CameraShake cameraShake;
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            StartCoroutine(cameraShake.Shake(.15f,.4f));
+        }
+    }
+}
