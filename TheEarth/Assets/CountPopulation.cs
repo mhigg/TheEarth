dce5bdd0490ff4a19ad337e416c7119a7fb347fb
boyama <tekito@mail>
2019-11-06 17:30:00 +0900

    人口カウント

---
+++ TheEarth/Assets/CountPopulation.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountPopulation : MonoBehaviour
+{
+    public GameObject popObject = null;
+    public int billion;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        billion = 770000;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int bilTop = billion / 10000;
+        int bilBottom = billion - bilTop * 10000;
+
+        Text popText = popObject.GetComponent<Text>();
+        popText.text = bilTop.ToString() + "億" + bilBottom.ToString();
+
+        billion--;
+    }
+}
--- TheEarth/Assets/NewBehaviourScript.cs
+++ TheEarth/Assets/NewBehaviourScript.cs
@@ -16,13 +16,12 @@ public class NewBehaviourScript : MonoBehaviour
         Vector3 distance = new Vector3(2, 0, 0);
         if(Input.GetKey(KeyCode.LeftArrow))
         {
-            this.gameObject.transform.position -= distance;
+            gameObject.transform.position -= distance;
         }
 
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            this.gameObject.transform.position += distance;
+            gameObject.transform.position += distance;
         }
-
     }
 }
