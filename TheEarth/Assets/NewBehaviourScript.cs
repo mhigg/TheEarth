09f6179818390119efb70a786274c7d807884f9e
kimura <gfhfhfgfhfjgfhfhgf.com>
2019-11-06 11:13:24 +0900

    Squashed commit of the following:
    commit 4e157ac283dd859a80500903f755d513760c0f6b
    Author: boyama <tekito@mail>
    Date:   Wed Nov 6 10:08:06 2019 +0900
    
        planet earth+

---
+++ TheEarth/Assets/NewBehaviourScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 distance = new Vector3(2, 0, 0);
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            this.gameObject.transform.position -= distance;
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            this.gameObject.transform.position += distance;
+        }
+
+    }
+}
