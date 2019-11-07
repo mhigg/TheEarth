98c544117606aa5633e1754cbdefc839d0c13b7a
MARY <phunmung@gmail.com>
2019-11-07 11:58:15 +0900

    エフェクト

---
+++ TheEarth/Assets/Scripts/GameOver.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOver : MonoBehaviour
+{
+    public CameraShake cameraShake;
+    public GameObject player;
+    private AudioSource ExplosionSE;
+
+    void Start()
+    {
+        AudioSource[] audioSources = GetComponents<AudioSource>();
+        ExplosionSE = audioSources[0];
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameOverNow();
+        }
+    }
+
+    public void GameOverNow()
+    {
+        StartCoroutine(cameraShake.Shake(4f, 10f));
+        Vector3 playerPos = player.transform.position;
+        playerPos.x -= .3f;
+        playerPos.y -= .3f;
+        StartCoroutine(Particle(15, 150f, 4f, playerPos, player.transform.rotation));
+        ExplosionSE.PlayOneShot(ExplosionSE.clip);
+    }
+
+    IEnumerator Particle(int count, float randomRange, float duration, Vector3 pos, Quaternion rotation)
+    {
+        int spawnedCount = 0;
+        while (spawnedCount < count)
+        {
+            var particle = Instantiate(Resources.Load("Explosion loop_2 soft", typeof(GameObject)), 
+                new Vector3 (pos.x + Random.Range(-randomRange, randomRange),
+                pos.y + Random.Range(-randomRange, randomRange),
+                pos.z + Random.Range(-randomRange, randomRange)), 
+                rotation) as GameObject;
+            particle.transform.localScale = new Vector3(100, 100, 100);
+            Destroy(particle, duration + (float)count / 10);
+
+            //数を加算する
+            spawnedCount++;
+
+            //UPDATE()と同時に行う
+            yield return null;
+        }
+    }
+}
