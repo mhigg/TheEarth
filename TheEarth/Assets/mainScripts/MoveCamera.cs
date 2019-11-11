b412a175c3f42063e9a3d66098844d0c24daee06
boyama <tekito@mail>
2019-11-11 19:17:23 +0900

    カメラの回転

--- TheEarth/Assets/mainScripts/MoveCamera.cs
+++ TheEarth/Assets/mainScripts/MoveCamera.cs
@@ -6,38 +6,17 @@ public class MoveCamera : MonoBehaviour
 {
     public GameObject Camera;
     public GameObject Earth;
-
-    private float pubRadius;
-    private float myRadius;
-    private float angleLate;
-
+    
     // Start is called before the first frame update
     void Start()
     {
-        //        Camera.transform.position = Earth.transform.position;
-        pubRadius = -1330.0f;
-        myRadius = -(Camera.transform.localPosition - Earth.transform.localPosition).magnitude;
-        angleLate = 0.0f;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.LeftArrow))
-        {
-            angleLate += 1.0f;
-            float angle2 = (angleLate * (Mathf.PI / 180.0f));
-            Camera.transform.position = new Vector3(
-                myRadius * Mathf.Sin(angle2),
-                Camera.transform.position.y,
-                myRadius * Mathf.Cos(angle2));
-        }
-
-        float angle = ((-Time.frameCount / 10.0f) * (Mathf.PI / 180.0f));
-        Camera.transform.position = new Vector3(
-            pubRadius * Mathf.Sin(angle),
-            Camera.transform.position.y,
-            pubRadius * Mathf.Cos(angle));
+        Camera.transform.RotateAround(Earth.transform.position, Vector3.down, Time.deltaTime * 20.0f);
 
         Camera.transform.LookAt(Earth.transform);
     }
---
+++ TheEarth/Assets/mainScripts/RotateCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCamera : MonoBehaviour
+{
+    public Transform Camera;
+    public GameObject Sun;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Camera.transform.LookAt(Sun.transform);
+    }
+}
